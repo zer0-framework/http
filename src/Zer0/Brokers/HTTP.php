c117825811cdee662230a34f68a0236a92e1b608
Vasily Zorin <kak.serpom.po.yaitsam@gmail.com>
2019-04-17 17:59:22 +0800

    Typing

--- src/Zer0/Brokers/HTTP.php
+++ src/Zer0/Brokers/HTTP.php
@@ -19,4 +19,14 @@ class HTTP extends Base
     {
         return new \Zer0\HTTP\HTTP($config, $this->app);
     }
+
+    /**
+     * @param string $name
+     * @param bool $caching
+     * @return \Zer0\HTTP\HTTP
+     */
+    public function get(string $name = '', bool $caching = true): \Zer0\HTTP\HTTP
+    {
+        return parent::get($name, $caching);
+    }
 }

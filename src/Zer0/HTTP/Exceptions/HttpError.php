2f8e1a815e3382d0502804a4c1119c7e569392fb
Vasily Zorin <kak.serpom.po.yaitsam@gmail.com>
2021-05-10 14:22:48 +0300

    HttpError

--- src/Zer0/HTTP/Exceptions/HttpError.php
+++ src/Zer0/HTTP/Exceptions/HttpError.php
@@ -4,12 +4,18 @@ namespace Zer0\HTTP\Exceptions;
 
 /**
  * Class HttpError
+ *
  * @package Zer0\HTTP\Exceptions
  */
-abstract class HttpError extends \Exception
+class HttpError extends \Exception
 {
     /**
      * @var int
      */
     public $httpCode;
+
+    public function setCode (int $code)
+    {
+        $this->httpCode = $code;
+    }
 }
